978f8c1994d03820a56fc2edeee0f3ba56f27ac7
NessDisk <ness.cero1@gmail.com>
2022-05-10 15:01:11 -0500

    Firebase-DB

---
+++ whatsapp-cl/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-5Et2GEgGAdPR_4zgo7ftS0aJEdyC3Dw",
+    authDomain: "whats-app-cl-3944a.firebaseapp.com",
+    projectId: "whats-app-cl-3944a",
+    storageBucket: "whats-app-cl-3944a.appspot.com",
+    messagingSenderId: "905327416571",
+    appId: "1:905327416571:web:5ccc901eea898e31714c5e",
+    measurementId: "G-50NXRLHC7H"
+  };
