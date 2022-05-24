fabf02429c4e5e2f9b71da7be0812fc130e2ece6
NessDisk <ness.cero1@gmail.com>
2022-05-24 00:16:50 -0500

    landscape

--- whatsapp-cl/src/firebase.js
+++ whatsapp-cl/src/firebase.js
@@ -18,15 +18,26 @@ const {REACT_APP_APIKEY,REACT_APP_AUTHDOMAIN, REACT_APP_DATABASEURL , REACT_APP_
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // Realtime Database
 
+// const firebaseConfig = {
+//   apiKey: REACT_APP_APIKEY,
+//   authDomain:  REACT_APP_AUTHDOMAIN,
+//   databaseURL: REACT_APP_DATABASEURL,
+//   projectId: REACT_APP_PROJECTID,
+//   storageBucket: REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+//   appId: REACT_APP_APPID,
+//   measurementId:  REACT_APP_MEASUREMENTID
+// };
+
 const firebaseConfig = {
-  apiKey: REACT_APP_APIKEY,
-  authDomain:  REACT_APP_AUTHDOMAIN,
-  databaseURL: REACT_APP_DATABASEURL,
-  projectId: REACT_APP_PROJECTID,
-  storageBucket: REACT_APP_STORAGEBUCKET,
-  messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-  appId: REACT_APP_APPID,
-  measurementId:  REACT_APP_MEASUREMENTID
+  apiKey: "AIzaSyA-5Et2GEgGAdPR_4zgo7ftS0aJEdyC3Dw",
+  authDomain: "whats-app-cl-3944a.firebaseapp.com",
+  databaseURL: "https://whats-app-cl-3944a-default-rtdb.firebaseio.com",
+  projectId: "whats-app-cl-3944a",
+  storageBucket: "whats-app-cl-3944a.appspot.com",
+  messagingSenderId: "905327416571",
+  appId: "1:905327416571:web:5ccc901eea898e31714c5e",
+  measurementId: "G-50NXRLHC7H"
 };
 
 // Initialize Firebase
